41dd292d1aabdfee78f825e964ea6100f3570928
HorribleCavalry <HordrenHan@outlook.com>
2021-01-23 18:35:48 +0800

    [Add] Init Terraria Project.

---
+++ Source/Terraria/TerrariaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TerrariaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TERRARIA_API ATerrariaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
