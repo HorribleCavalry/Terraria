f8e759c7ae80e83ed3fe946f4a2b36f4d56a7071
Han Shu Bin <HordrenHan@outlook.com>
2021-03-02 22:18:30 +0800

    [Add] Added Blocks Plugin

---
+++ Plugins/Blocks/Source/Blocks/Private/BlockBase.cpp
@@ -0,0 +1,65 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlockBase.h"
+
+// Sets default values
+ABlockBase::ABlockBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+	InitializeBlockMesh();
+	InitializeBlockBlockMaterial();
+	InitializeBlockProperity();
+}
+
+// Called when the game starts or when spawned
+void ABlockBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ABlockBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABlockBase::OnBlockIdChanged()
+{
+}
+
+void ABlockBase::OnBlockPlanted()
+{
+}
+
+void ABlockBase::OnBlockDestroyed()
+{
+}
+
+void ABlockBase::OnCharacterTouched()
+{
+}
+
+void ABlockBase::OnCharacterDigged()
+{
+}
+
+void ABlockBase::OnBulletHitted()
+{
+}
+
+void ABlockBase::InitializeBlockMesh()
+{
+}
+
+void ABlockBase::InitializeBlockBlockMaterial()
+{
+}
+
+void ABlockBase::InitializeBlockProperity()
+{
+	BlockBaseContentPath = TEXT("/Blocks/Content/Blocks");
+}
+
---
+++ Plugins/Blocks/Source/Blocks/Public/BlockBase.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BlockBase.generated.h"
+
+UCLASS()
+class BLOCKS_API ABlockBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABlockBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	FString BlockBaseContentPath;
+public:
+	UPROPERTY(EditAnywhere)
+	uint32 BlockID;
+
+	UPROPERTY(EditAnywhere)
+	bool bMobility;
+
+	UPROPERTY(EditAnywhere)
+	uint32 Infectivity;
+
+	UPROPERTY(EditAnywhere)
+	bool bCanBeInfected;
+
+public:
+	virtual void OnBlockIdChanged();
+	virtual void OnBlockPlanted();
+	virtual void OnBlockDestroyed();
+	virtual void OnCharacterTouched();
+	virtual void OnCharacterDigged();
+	virtual void OnBulletHitted();
+
+public:
+	virtual void InitializeBlockMesh();
+	virtual void InitializeBlockBlockMaterial();
+	virtual void InitializeBlockProperity();
+};
